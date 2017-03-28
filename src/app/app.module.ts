068f913c5d53fb1e261431c4e5f147c973cb9d0c
rodrigo-delgado <rod.delgado85@gmail.com>
2017-03-28 15:28:50 +1300

    add router and routerLink

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppComponent }  from './app.component';
 import { AboutComponent }  from './components/about/about.component';
 import { NavbarComponent }  from './components/navbar/navbar.component';
 import { SearchComponent }  from './components/search/search.component';
+import { routing }  from './app.routing';
 
 @NgModule({
-  imports:      [ BrowserModule ],
+  imports:      [ BrowserModule, routing ],
 
   declarations: [ AppComponent,
                   AboutComponent,
