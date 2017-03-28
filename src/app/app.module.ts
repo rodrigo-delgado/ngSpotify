3b11b7595f96f891645baed13ca3880622b737fe
rodrigo-delgado <rod.delgado85@gmail.com>
2017-03-28 15:08:54 +1300

    add bootsrap navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
+import { AboutComponent }  from './components/about/about.component';
+import { NavbarComponent }  from './components/navbar/navbar.component';
+import { SearchComponent }  from './components/search/search.component';
 
 @NgModule({
   imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+
+  declarations: [ AppComponent,
+                  AboutComponent,
+                  NavbarComponent,
+                  SearchComponent ],
+
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
