32ab6eb7a6654698fd1fa9cea5e891df0c887e71
rodrigo-delgado <rod.delgado85@gmail.com>
2017-03-29 13:29:53 +1300

    add spotify services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent }  from './app.component';
 import { AboutComponent }  from './components/about/about.component';
@@ -8,7 +9,7 @@ import { SearchComponent }  from './components/search/search.component';
 import { routing }  from './app.routing';
 
 @NgModule({
-  imports:      [ BrowserModule, routing ],
+  imports:      [ BrowserModule, routing, FormsModule ],
 
   declarations: [ AppComponent,
                   AboutComponent,
